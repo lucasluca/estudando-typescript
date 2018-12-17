0737ed2351acfac1a97676922c693dbe3981f948
Lucas Augusto Mendes Rodrigues <lucas.augustus@gmail.com>
2018-12-17 16:25:55 -0200

    Traversy Media Crash Course

---
+++ TraversyCrashCourse/types.ts
@@ -0,0 +1,33 @@
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;
+
+
+let strArr:string[];
+let numArr: number[];
+let boolArr: boolean[];
+
+
+// let strArr: Array<string>;
+// let numArr: Array<number>;
+// let boolArr: Array<boolean>;
+
+let strNumTuple: [string, number];
+
+myString = 'Hello'.slice(0,3);
+myNum = 1;
+myBool = false;
+myVar = true;
+
+strArr = ['Hello', 'World'];
+numArr = [1,2,3];
+boolArr = [true, false, true];
+
+strNumTuple = ['Hello', 4];
+
+let myVoid: void = undefined;
+let myNull: null = null;
+let myUndefined: undefined = null;
+
+console.log(strArr);
